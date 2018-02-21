d6d0fb1fb6c9b6757a98f7e20bde452636df3a7b
Robot <a.robot.n@gmail.com>
2018-02-22 01:42:37 +0800

    no message

--- TZGameServer/settings.py
+++ TZGameServer/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
