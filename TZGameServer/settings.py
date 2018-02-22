90c6f8eefbd0c6eef204e9764fa5cc2bd3c5a804
Robot <a.robot.n@gmail.com>
2018-02-22 15:30:27 +0800

    no message

--- TZGameServer/settings.py
+++ TZGameServer/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'rjqsjb$!195!7rg=zih(nkcp7o&9424yk2r#v#f-a_n#+jr+o-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
